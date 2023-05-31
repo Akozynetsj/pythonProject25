2ada9696fe85555e0c3efd4652024dadb67716b0
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 21:05:48 +0300

    ex 1

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+def join_parameters(*args):
+    return ', '.join(str(arg) for arg in args)
+result = join_parameters('apple', 'banana', 'cherry', 'date')
+print(f'Рядок з об\'єднаними параметрами: {result}')
 
