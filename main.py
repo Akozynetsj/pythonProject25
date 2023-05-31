8984dea21f54538ec7d6b9287d4ece1ff8a40889
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 21:07:39 +0300

    ex 2

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def join_parameters(*args):
-    return ', '.join(str(arg) for arg in args)
-result = join_parameters('apple', 'banana', 'cherry', 'date')
-print(f'Рядок з об\'єднаними параметрами: {result}')
+def find_min_max(*args):
+    if len(args) == 0:
+        return None
+
+    min_value = min(args)
+    max_value = max(args)
+    return min_value, max_value
+result = find_min_max(10, 5, 8, 12, 3)
+print(f'Найменше та найбільше значення: {result}')
+
+result = find_min_max(7, 2, 9, 1, 6, 4)
+print(f'Найменше та найбільше значення: {result}')
 
