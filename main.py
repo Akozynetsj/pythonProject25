13eba9a3082fe81ce95322eacb71dde2f479d9a9
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 21:09:11 +0300

    ex 3

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-def find_min_max(*args):
-    if len(args) == 0:
-        return None
-
-    min_value = min(args)
-    max_value = max(args)
-    return min_value, max_value
-result = find_min_max(10, 5, 8, 12, 3)
-print(f'Найменше та найбільше значення: {result}')
-
-result = find_min_max(7, 2, 9, 1, 6, 4)
-print(f'Найменше та найбільше значення: {result}')
-
+def create_squared_dict(**kwargs):
+    squared_dict = {}
+    for key, value in kwargs.items():
+        squared_dict[key] = value ** 2
+    return squared_dict
+result = create_squared_dict(a=2, b=3, c=4)
+print(f'Словник з квадратами значень: {result}')
